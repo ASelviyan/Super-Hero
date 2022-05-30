aef458d0fc7a45e47b042e8d9436059d22b97223
ASelviyan <angelikaselviyan5@gmail.com>
2022-05-29 23:32:49 -0600

    completed the search and deatils pages

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -1,9 +1,16 @@
+const { default: axios } = require('axios')
 const express = require('express')
 const router = express.Router()
-
-router.get('/list', (req, res) =>{
-  
+router.get('/list', (req, res) => {
   res.render('list/index.ejs')
+})
+
+
+router.post('/list', async(req, res) =>{
+  let heroURL = `https://superheroapi.com/api/${process.env.API_KEY}/search/${req.body.name}`
+  const heroesData = await axios.get(heroURL)
+  console.log(heroesData.data.results[0])
+  res.render('list/details.ejs', {heroesData: heroesData.data.results[0]})
 })
 
 router.get('/details', (req, res) =>{
